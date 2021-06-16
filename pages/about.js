746353ae5a4a6d5be666d5180351f32921b4a59c
Warner Witt <warner.witt@projekt202.com>
2021-06-16 09:09:49 -0700

    title

--- pages/about.js
+++ pages/about.js
@@ -12,7 +12,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="About Us" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
